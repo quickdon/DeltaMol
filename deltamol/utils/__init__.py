2f12b500561baa88396da1b7de8b78baf3686ee8
ZHOU SHIJIA <145174881+quickdon@users.noreply.github.com>
2025-09-24 15:50:29 +0800

    Add reproducible seeding and configurable initialisation

--- deltamol/utils/__init__.py
+++ deltamol/utils/__init__.py
@@ -1,4 +1,21 @@
 """Utility helpers for DeltaMol."""
+from .distributed import (
+    DistributedConfig,
+    DistributedState,
+    get_distributed_state,
+    init_distributed,
+    is_main_process,
+)
 from .logging import configure_logging
+from .random import seed_everything, seed_worker
 
-__all__ = ["configure_logging"]
+__all__ = [
+    "DistributedConfig",
+    "DistributedState",
+    "configure_logging",
+    "get_distributed_state",
+    "init_distributed",
+    "is_main_process",
+    "seed_everything",
+    "seed_worker",
+]
