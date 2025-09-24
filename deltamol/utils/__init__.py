0a9e3c5d7140d34bdc8c66a64a6be1abece2c0c5
ZHOU SHIJIA <145174881+quickdon@users.noreply.github.com>
2025-09-24 15:07:00 +0800

    Add configurable batch logging and tensorboard support

--- deltamol/utils/__init__.py
+++ deltamol/utils/__init__.py
@@ -1,4 +1,18 @@
 """Utility helpers for DeltaMol."""
+from .distributed import (
+    DistributedConfig,
+    DistributedState,
+    get_distributed_state,
+    init_distributed,
+    is_main_process,
+)
 from .logging import configure_logging
 
-__all__ = ["configure_logging"]
+__all__ = [
+    "DistributedConfig",
+    "DistributedState",
+    "configure_logging",
+    "get_distributed_state",
+    "init_distributed",
+    "is_main_process",
+]
