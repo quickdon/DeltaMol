1d8a39dcf83b9c0866d465bb3c6992b21afae3c8
ZHOU SHIJIA <145174881+quickdon@users.noreply.github.com>
2025-12-06 03:09:31 +0800

    Add distributed process group cleanup

--- deltamol/utils/__init__.py
+++ deltamol/utils/__init__.py
@@ -2,6 +2,7 @@
 from .distributed import (
     DistributedConfig,
     DistributedState,
+    destroy_process_group,
     get_distributed_state,
     init_distributed,
     is_main_process,
@@ -13,6 +14,7 @@ __all__ = [
     "DistributedConfig",
     "DistributedState",
     "configure_logging",
+    "destroy_process_group",
     "get_distributed_state",
     "init_distributed",
     "is_main_process",
